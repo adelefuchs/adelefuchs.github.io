7dec09ebf9d1414e54b072b30667f54c439715e8
adelefuchs <123142789+adelefuchs@users.noreply.github.com>
2024-09-09 17:29:21 -0400

    no scrollspy

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Add any interactivity you want here
 document.addEventListener('DOMContentLoaded', () => {
-    // Example: Smooth scrolling for navigation links
     document.querySelectorAll('nav ul li a').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
@@ -27,25 +25,25 @@ document.addEventListener("DOMContentLoaded", function () {
     typeWriter();
 });
 
-// Scrollspy effect
-window.addEventListener('scroll', function () {
-    let sections = document.querySelectorAll('section');
-    let navLinks = document.querySelectorAll('nav ul li a');
+// // Scrollspy effect
+// window.addEventListener('scroll', function () {
+//     let sections = document.querySelectorAll('section');
+//     let navLinks = document.querySelectorAll('nav ul li a');
 
-    sections.forEach(section => {
-        let top = window.scrollY;
-        let offset = section.offsetTop - 150;
-        let height = section.offsetHeight;
-        let id = section.getAttribute('id');
+//     sections.forEach(section => {
+//         let top = window.scrollY;
+//         let offset = section.offsetTop - 150;
+//         let height = section.offsetHeight;
+//         let id = section.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                document.querySelector('nav ul li a[href*=' + id + ']').classList.add('active');
-            });
-        }
-    });
-});
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach(link => {
+//                 link.classList.remove('active');
+//                 document.querySelector('nav ul li a[href*=' + id + ']').classList.add('active');
+//             });
+//         }
+//     });
+// });
 
 // const imageCount = 9;
 // let currentImageIndex = 1;
